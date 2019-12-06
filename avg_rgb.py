cca51735ac3a90b7c313766303d98d10d5b557d5
samblack15 <sam.black@temple.edu>
2019-12-06 17:11:38 -0500

    init commit

--- avg_rgb.py
+++ avg_rgb.py
@@ -3,10 +3,19 @@ import numpy as np
 
 means = []
 for i in range(10000):
-	img = cv2.imread('checkpoints/pretrained-cifar/output/{}.jpg'.format(i))
+	img = cv2.imread('checkpoints/pretrained-cifar-blue2/output/{}.jpg'.format(i))
 	try:
-		means.append(np.mean(img, axis=(0, 1)))
+		means.append(np.mean(abs(np.array([209, 172, 48]) - img)))
 	except Exception:
 		break
 
-print(np.mean(means, axis=0))
+baseline_means = []
+for i in range(10000):
+	img = cv2.imread('checkpoints/pretrained-cifar/output/{}.jpg'.format(i))
+	try:
+		baseline_means.append(np.mean(abs(np.array([209, 172, 48]) - img)))
+	except Exception:
+		break
+
+print(np.mean(means))
+print(np.mean(baseline_means))
